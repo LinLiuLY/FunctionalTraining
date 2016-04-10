e0b3b8725cfaaefd030f3428b163d57961ca4d17
Lin Liu <liulin1987315@gmail.com>
2016-04-10 14:53:26 +0800

    compose different function to a new function

--- src/main/scala/com/rea/higherorder/ComposingExercises.scala
+++ src/main/scala/com/rea/higherorder/ComposingExercises.scala
@@ -1,29 +1,35 @@
 package com.rea.higherorder
 
 object ComposingExercises {
-
+  //compose makes a new function that composes other functions f(g(x))
+  //val fComposeG = f _ compose g _
 
   def add(a: Int, b: Int) = a + b
 
   // 1. We want to partially apply add
-  def addTwo: Int => Int = a => add(a, 2)
+  def addTwo: Int => Int = add(_, 2)
 
   println(addTwo(5))
 
   def subtract(a: Int, b: Int) = a - b
 
   // 2. We want to partially apply subtract
-  def minusThree: Int => Int = a => subtract(a, 3)
+  def minusThree: Int => Int = subtract(_, 3)
 
   println(minusThree(5))
 
   // 3. How do we create a new function from addTwo and minusThree?
-  val addTwoMinusThree: Int => Int = a => addTwo(a) + minusThree(a)
+  val addTwoMinusThree: Int => Int = addTwo _ compose minusThree _
+
+  println(addTwoMinusThree(5))
+
   
   def prettyPrint(i:Int) = s"The number is $i"
 
   //4.
-  val addTwoMinusThreePrettyPrint: Int => String = a => prettyPrint(addTwoMinusThree(a))
+  val addTwoMinusThreePrettyPrint: Int => String = prettyPrint _ compose addTwoMinusThree _
+
+  println(addTwoMinusThreePrettyPrint(9))
 
   // 5. How do we convert (compose) f and g into a brand new function?
   // def compose_[A, B, C](f: B => C, g: A => B): A => C = ???
