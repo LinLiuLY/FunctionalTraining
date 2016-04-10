9c3c88b04d97d840ce666e43403af2efdca69fcc
Lin Liu <liulin1987315@gmail.com>
2016-04-10 16:43:19 +0800

    add compose description in the exercises

--- src/main/scala/com/rea/higherorder/ComposingExercises.scala
+++ src/main/scala/com/rea/higherorder/ComposingExercises.scala
@@ -4,6 +4,21 @@ object ComposingExercises {
   //compose makes a new function that composes other functions f(g(x))
   //val fComposeG = f _ compose g _
 
+  //def addUmm(x: String) = x + " umm"
+  //def addAhem(x: String) = x + " ahem"
+  //val ummThenAhem = addAhem _ compose addUmm
+
+  //addAhem is a method. compose method is defined on functions. 
+  //addAhem _ converts addAhem from method to function, so compose can be called on it. 
+  //compose expects a function as it's argument. 
+  //You are giving it a method addUmm by converting addUmm into a function with addUmm _ 
+
+  //(The underscore can be left out because the compiler can automatically convert a method into a function when it knows that a function is expected anyway)
+  //addUhum _
+  //is an eta expansion. It converts methods into functions.
+
+
+
   def add(a: Int, b: Int) = a + b
 
   // 1. We want to partially apply add
@@ -19,7 +34,7 @@ object ComposingExercises {
   println(minusThree(5))
 
   // 3. How do we create a new function from addTwo and minusThree?
-  val addTwoMinusThree: Int => Int = addTwo _ compose minusThree _
+  val addTwoMinusThree: Int => Int = addTwo compose minusThree
 
   println(addTwoMinusThree(5))
 
@@ -27,7 +42,7 @@ object ComposingExercises {
   def prettyPrint(i:Int) = s"The number is $i"
 
   //4.
-  val addTwoMinusThreePrettyPrint: Int => String = prettyPrint _ compose addTwoMinusThree _
+  val addTwoMinusThreePrettyPrint: Int => String = prettyPrint _ compose addTwoMinusThree
 
   println(addTwoMinusThreePrettyPrint(9))
 
