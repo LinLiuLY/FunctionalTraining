58c40c599165dee4897a4495959a5dd89cfd3a92
Lin Liu <liulin1987315@gmail.com>
2016-04-10 14:40:51 +0800

    do the RecursionExercises

--- src/main/scala/com/rea/higherorder/ComposingExercises.scala
+++ src/main/scala/com/rea/higherorder/ComposingExercises.scala
@@ -5,23 +5,32 @@ object ComposingExercises {
 
   def add(a: Int, b: Int) = a + b
 
-  // We want to partially apply add
-  def addTwo: Int => Int = ???
+  // 1. We want to partially apply add
+  def addTwo: Int => Int = a => add(a, 2)
+
+  println(addTwo(5))
 
   def subtract(a: Int, b: Int) = a - b
 
-  // We want to partially apply subtract
-  def minusThree: Int => Int = ???
+  // 2. We want to partially apply subtract
+  def minusThree: Int => Int = a => subtract(a, 3)
 
-  // How do we create a new function from addTwo and minusThree?
-  val addTwoMinusThree: Int => Int = ???
+  println(minusThree(5))
+
+  // 3. How do we create a new function from addTwo and minusThree?
+  val addTwoMinusThree: Int => Int = a => addTwo(a) + minusThree(a)
   
   def prettyPrint(i:Int) = s"The number is $i"
-  
-  val addTwoMinusThreePrettyPrint: Int => String = ???
 
-  // How do we convert (compose) f and g into a brand new function?
-  def compose_[A, B, C](f: B => C, g: A => B): A => C = ???
+  //4.
+  val addTwoMinusThreePrettyPrint: Int => String = a => prettyPrint(addTwoMinusThree(a))
+
+  // 5. How do we convert (compose) f and g into a brand new function?
+  // def compose_[A, B, C](f: B => C, g: A => B): A => C = ???
+
+  def compose_[A, B, C](f: B => C, g: A => B): A => C = {
+    x:A => f(g(x))
+  } 
 
 }
 
